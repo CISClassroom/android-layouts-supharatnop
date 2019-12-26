bcff5ad6a2cf95a5fd7b29e13d006c58f511c771
supharatnop <33540580+supharatnop@users.noreply.github.com>
2019-12-27 01:11:23 +0700

    update Constrant

--- app/src/main/java/com/cis/lab/androoidlayouts/ConstrantActivity.kt
+++ app/src/main/java/com/cis/lab/androoidlayouts/ConstrantActivity.kt
@@ -10,8 +10,6 @@ class ConstrantActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constrant)
 
-        button6.setOnClickListener{
-            this.finish()
-        }
+
     }
 }
